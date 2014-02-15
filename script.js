a5ba190f00a617e26bf85a24d7648517fde9fc3a
billsuxx <scottanyo@gmail.com>
2014-02-15 10:49:48 +0100

    Create script.js
    init

---
+++ script.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Tchibo Affiliate Marketing Group Selector Fix
+// @namespace    https://affiliate.tchibo.hu
+// @version      0.1
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @description  Ha mar unod, hogy a form elfelejti az aktualis marketing csoportot
+// @match        https://affiliate.tchibo.hu/idevaffiliate/account.php*
+// @copyright    2014 billsuxx http://github.com/billsuxx
+// ==/UserScript==
+
+(function() {
+  
+    var bannerIdContainer = jQuery('html').find('.tartalom b font')[2], //csak ilyen randa selectorral tudtam megtalalni az eppen aktualisan kivalasztott bannert
+    bannerId = $(bannerIdContainer).text();
+
+    if (bannerId) {    
+        jQuery(".tartalom select").find("option").filter(function(index) {
+            return bannerId === $(this).text();
+        }).prop("selected", "selected");
+    }
+
+})();
